70597018b0266d7e2ef290f3a4e51219822a2ca6
蒼時弦也 <elct9620@frost.tw>
2021-02-21 12:47:34 +0800

    Define initial calculator test case

--- spec/budget_calculator_spec.rb
+++ spec/budget_calculator_spec.rb
@@ -3,5 +3,14 @@
 require 'budget_calculator'
 
 RSpec.describe BudgetCalculator do
-  pending 'invalid data range'
+  let(:repo) { instance_double(BudgetRepo) }
+  subject(:calculator) { described_class.new(repo) }
+
+  describe '#query' do
+    let(:start_at) { Date.parse('2021/1/1') }
+    let(:end_at) { Date.parse('2021/1/1') }
+    subject(:query) { calculator.query(start_at, end_at) }
+
+    it { is_expected.to be(0) }
+  end
 end
